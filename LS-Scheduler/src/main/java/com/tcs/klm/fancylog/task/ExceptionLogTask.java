8e73372f44ae6e82cd593932faaa59ca50dd7da7
vinod <Kumar.PentaVinod@klm.com>
2014-06-28 21:48:45 +0200

    Exception analysis to parallel from serial.

--- LS-Scheduler/src/main/java/com/tcs/klm/fancylog/task/ExceptionLogTask.java
+++ LS-Scheduler/src/main/java/com/tcs/klm/fancylog/task/ExceptionLogTask.java
@@ -43,7 +43,7 @@ public class ExceptionLogTask {
     private String COLLECTION_EXCEPTION_COUNT = "exceptioncount";
 
     public void perfoemTask() {
-        System.out.println("ExceptionLogTask");
+        APPLICATION_LOGGER.info("ExceptionLogTask started...");
         DBCollection settingsCollection = mongoTemplate.getCollection(COLLECTION_SETTINGS);
         DBCursor settingsCursor = settingsCollection.find();
         Calendar calendar = Calendar.getInstance();
@@ -106,11 +106,10 @@ public class ExceptionLogTask {
                             task = new ExceptionAnalysisThread(file, sessionIDPossition, mongoTemplate);
                             Thread thread = new Thread(task);
                             thread.start();
-                            thread.join();
                             threads.add(thread);
                         }
                         for (Thread thread : threads) {
-
+                            thread.join();
                         }
 
                     }
@@ -122,8 +121,8 @@ public class ExceptionLogTask {
                 }
 
             }
+            APPLICATION_LOGGER.info("Exception log download completed.. ");
             Map<String, ExceptionBean> exceptionBeanMap = new HashMap<String, ExceptionBean>();
-
             BasicDBObject searchQuery = new BasicDBObject();
             searchQuery.put("date", date);
             DBCollection collection = mongoTemplate.getCollection(COLLECTION_EXCEPTION);
@@ -146,6 +145,7 @@ public class ExceptionLogTask {
                     ExceptionBean exceptionBean1 = new ExceptionBean();
                     exceptionBean1.setClassName(className);
                     exceptionBean1.setException(exception);
+                    exceptionBean1.setDate(date);
                     exceptionBeanMap.put(key, exceptionBean1);
                 }
             }
@@ -158,6 +158,7 @@ public class ExceptionLogTask {
                 }
             }
             mongoTemplate.insertAll(exceptionBeans);
+            APPLICATION_LOGGER.info("ExceptionBean update completed...");
 
         }
     }
@@ -173,7 +174,7 @@ public class ExceptionLogTask {
         if (listHyeperLink != null && !listHyeperLink.isEmpty()) {
             try {
                 Runnable task;
-                APPLICATION_LOGGER.info("Download Started... {}", System.currentTimeMillis());
+                APPLICATION_LOGGER.info("Download Started... ");
                 List<Thread> threads = new ArrayList<Thread>();
                 /*
                  * ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor(); taskExecutor.setCorePoolSize(4); taskExecutor.setMaxPoolSize(20); taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
@@ -188,6 +189,7 @@ public class ExceptionLogTask {
                 for (Thread thread : threads) {
                     thread.join();
                 }
+                APPLICATION_LOGGER.info("Download ended... ");
             }
             catch (InterruptedException e) {
                 // TODO Auto-generated catch block
